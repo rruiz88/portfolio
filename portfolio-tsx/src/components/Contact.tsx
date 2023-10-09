b2c18702af5cd11d281e536ebfa4c6032c834192
rruiz88 <roman.ruiz816@gmail.com>
2023-10-09 15:16:10 -0500

    form submit completed using formspree and hamburger menu completed

--- portfolio-tsx/src/components/Contact.tsx
+++ portfolio-tsx/src/components/Contact.tsx
@@ -1,9 +1,22 @@
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xeqbvjvp");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  if (state.succeeded) {
+    return (
+      <p className="text-green-400 text-center">Thanks for the message!</p>
+    );
+  }
+
   return (
     <section id="contact" className="relative">
       <form
-        netlify
         name="contact"
+        onSubmit={handleSubmit}
         className="flex flex-col border rounded-xl justify-center items-center mt-10"
       >
         <h2 className="text-blue-400 sm:text-4xl text-3xl mb-1 font-bold">
@@ -20,6 +33,7 @@ const Contact = () => {
             name="name"
             placeholder="Your name"
             className="w-full bg-white rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="relative mb-4">
@@ -32,7 +46,9 @@ const Contact = () => {
             placeholder="Your email"
             name="email"
             className="w-full bg-white rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            onChange={(e) => setEmail(e.target.value)}
           />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
         <div className="relative mb-4">
           <label htmlFor="message" className="leading-7 text-sm text-gray-400">
@@ -42,11 +58,18 @@ const Contact = () => {
             id="message"
             name="message"
             placeholder="Your message"
-            className="w-full bg-white rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-black py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
           />
         </div>
         <button
           type="submit"
+          disabled={state.submitting}
           className="text-white bg-green-400 border-0 py-2 px-6 mb-2 focus:outline-none hover:bg-indigo-900 rounded text-lg"
         >
           Submit
--- portfolio-tsx/src/components/Navbar.tsx
+++ portfolio-tsx/src/components/Navbar.tsx
@@ -1,4 +1,12 @@
 const Navbar = () => {
+  const navToggle = () => {
+    const btn = document.getElementById("menu-btn");
+    const menu = document.getElementById("menu");
+    btn?.classList.toggle("open");
+    menu?.classList.toggle("flex");
+    menu?.classList.toggle("hidden");
+  };
+
   return (
     <nav className="container relative mx-auto p-6">
       {/* <!-- Flex Container For Nav Items --> */}
@@ -36,7 +44,9 @@ const Navbar = () => {
         {/* <!-- Hamburger Button --> */}
         <button
           id="menu-btn"
+          type="button"
           className="z-30 block md:hidden focus:outline-none hamburger"
+          onClick={navToggle}
         >
           <span className="hamburger-top"></span>
           <span className="hamburger-middle"></span>
@@ -46,20 +56,25 @@ const Navbar = () => {
       {/* <!-- Mobile Menu --> */}
       <div
         id="menu"
-        className="fixed inset-0 z-20 hidden flex-col items-center self-end w-full h-full m-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y divide-gray-500 opacity-90 bg-veryDarkBlue"
+        className="fixed inset-0 z-20 hidden flex-col items-center self-end w-full h-full m-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y divide-gray-500 opacity-90 bg-gray-600"
       >
         <div className="w-full py-3 text-center">
-          <a href="#about" className="block hover:text-white">
+          <a href="#about" className="block hover:text-green-400">
             About
           </a>
         </div>
         <div className="w-full py-3 text-center">
-          <a href="#projects" className="block hover:text-white">
+          <a href="#skills" className="block hover:text-green-400">
+            Skills
+          </a>
+        </div>
+        <div className="w-full py-3 text-center">
+          <a href="#projects" className="block hover:text-green-400">
             Projects
           </a>
         </div>
         <div className="w-full py-3 text-center">
-          <a href="#contact" className="block hover:text-white">
+          <a href="#contact" className="block hover:text-green-400">
             Contact
           </a>
         </div>
